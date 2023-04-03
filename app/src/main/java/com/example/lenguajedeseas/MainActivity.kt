de1a957f57a91f31435672a273f0277bc1431d45
ulisesten <ulises.mtz.el@gmail.com>
2023-04-03 14:13:40 -0600

    update

--- app/src/main/java/com/example/lenguajedeseas/MainActivity.kt
+++ app/src/main/java/com/example/lenguajedeseas/MainActivity.kt
@@ -4,64 +4,67 @@ import android.Manifest
 import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
-//import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.lenguajedeseas.databinding.ActivityMainBinding
 import com.example.lenguajedeseas.ml.Model
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.common.InputImage
-//import com.google.mlkit.vision.label.ImageLabeling
-//import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import org.tensorflow.lite.support.image.TensorImage
-//import java.io.ByteArrayOutputStream
 import java.io.IOException
-//import java.nio.ByteBuffer
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-//typealias AnalysisListener = (image: InputImage) -> Unit
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity:AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
-    private var imageCapture: ImageCapture? = null
-    //private var imageAnalyzer: ImageAnalysis? = null
-    //private var objectDetector: ObjectDetector? = null
-
     private lateinit var cameraExecutor: ExecutorService
-    //private var localModel: LocalModel? = null
-    //private var customOptions: CustomObjectDetectorOptions? = null
+    private lateinit var context:Context
+    private var imageCapture: ImageCapture? = null
+    lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
+        context = this@MainActivity
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        imageCapture = ImageCapture.Builder()
+            .build()
+
         ///Camara
         if (allPermissionsGranted()) {
-            startCamera()
+            startCamera(viewBinding.viewFinder)
         } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        viewBinding.btnAnalizar.setOnClickListener { takePhoto() }
+        viewBinding.btnAnalizar.setOnClickListener {
+            takePhoto()
+        }
+
         cameraExecutor = Executors.newSingleThreadExecutor()
 
     }
 
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+    fun startCamera(viewFinder: PreviewView) {
 
         cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
@@ -71,19 +74,8 @@ class MainActivity : AppCompatActivity() {
             val preview = Preview.Builder()
                 .build()
                 .also {
-                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
                 }
-
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            /*imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, MyImageAnalyzer (this) { image: InputImage ->
-                        //Utils().debugInfo("Análisis", "Image callback")
-                    })
-                }*/
 
             // Select back camera as a default
             //val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
@@ -94,11 +86,13 @@ class MainActivity : AppCompatActivity() {
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
+
+
                 cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture/*, imageAnalyzer*/)
 
             } catch(exc: Exception) {
-                Utils().debugInfo(TAG, "Use case binding failed ${exc.message}")
+                Utils().debugInfo(ContentValues.TAG, "Use case binding failed ${exc.message}")
             }
 
         }, ContextCompat.getMainExecutor(this))
@@ -141,64 +135,47 @@ class MainActivity : AppCompatActivity() {
                     try {
                         image = InputImage.fromFilePath(this@MainActivity, output.savedUri as Uri)
 
-                        //myObjectLabeler(image)
-                        myObjectDetection(this@MainActivity,image)
+                        myObjectDetection(image)
 
                     } catch (e: IOException) {
                         e.printStackTrace()
                     }
-
                 }
             }
         )
     }
 
-    /*private fun myObjectLabeler(image: InputImage){
-        val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
-        var outputText = ""
-        labeler.process(image)
-            .addOnSuccessListener { labels ->
-                // Task completed successfully
-                for (label in labels) {
-                    val text = label.text
-                    val confidence = label.confidence
-                    outputText += "$text : $confidence\n"
-                }
-                Utils().debugInfo("labeling", outputText)
-                viewBinding.showDescription.text = outputText
-            }
-            .addOnFailureListener { e ->
-                // Task failed with an exception
-                e.printStackTrace()
-            }
-    }*/
 
-    private fun myObjectDetection(ctx: Context, image: InputImage) {
-        val model = Model.newInstance(ctx)
+
+    private fun myObjectDetection(image: InputImage) {
+        val model = Model.newInstance(context)
 
         val tfImage = TensorImage.fromBitmap(image.bitmapInternal)
         val outputs = model.process(tfImage)
         val detectionResult = outputs.detectionResultList[0]
 
-        // Gets result from DetectionResult.
-        //val score = detectionResult.scoreAsFloat
-        //val location = detectionResult.locationAsRectF
-        val category = detectionResult.categoryAsString
+        val location = detectionResult.locationAsRectF
+        Log.d(TAG, location.toString())
 
-        val text = "Número ${category.lowercase()} en lengua de señas"
-        viewBinding.showDescription.text = text
+        viewBinding.showDescription.text = detectionResult.categoryAsString
         model.close()
     }
+
+
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
+
+
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
     }
+
+
 
     companion object {
         private const val TAG = "CameraXApp"
@@ -221,7 +198,7 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                startCamera()
+                startCamera(viewBinding.viewFinder)
             } else {
                 Toast.makeText(this,
                     "Permissions not granted by the user.",
@@ -230,63 +207,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
-    /**private class MyImageAnalyzer(ctx: Context, private val listener: AnalysisListener)  : ImageAnalysis.Analyzer {
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        val model = Model.newInstance(ctx)
-
-        override fun analyze(imageProxy: ImageProxy) {
-            val buffer = imageProxy.planes[0].buffer
-            val data = buffer.toByteArray()
-
-            val image = InputImage.fromByteArray(
-                data,
-                480,
-                360,
-                imageProxy.imageInfo.rotationDegrees,
-                InputImage.IMAGE_FORMAT_NV21 // or IMAGE_FORMAT_YV12
-            )
-
-            val tfImage = TensorImage.fromBitmap(toBitmap(imageProxy))
-            val outputs = model.process(tfImage)
-            val detectionResult = outputs.detectionResultList[0]
-
-            // Gets result from DetectionResult.
-            val location = detectionResult.scoreAsFloat
-            val category = detectionResult.locationAsRectF
-            val score = detectionResult.categoryAsString
-
-            Utils().debugInfo("Model", "$location $category $score")
-
-            listener(image)
-            //model.close()
-            imageProxy.close()
-        }
-
-        fun toBitmap(imageProxy: ImageProxy): Bitmap {
-            val yBuffer = imageProxy.planes[0].buffer // Y
-            val vuBuffer = imageProxy.planes[2].buffer // VU
-
-            val ySize = yBuffer.remaining()
-            val vuSize = vuBuffer.remaining()
-
-            val nv21 = ByteArray(ySize + vuSize)
-
-            yBuffer.get(nv21, 0, ySize)
-            vuBuffer.get(nv21, ySize, vuSize)
-
-            val yuvImage = YuvImage(nv21, ImageFormat.NV21, imageProxy.width, imageProxy.height, null)
-            val out = ByteArrayOutputStream()
-            yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
-            val imageBytes = out.toByteArray()
-            return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-        }
-    }**/
 }
